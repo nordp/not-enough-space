5b2de789c9dd1f7cd9b64a41ff04c59c9fbb38dd
juliaortheden <orthedenjulia@gmail.com>
2017-03-30 19:57:34 +0200

    cow class 1st

--- src/main/java/edu/chalmers/notenoughspace/Cow.java
+++ src/main/java/edu/chalmers/notenoughspace/Cow.java
@@ -1,4 +1,12 @@
 package edu.chalmers.notenoughspace;
 
 public class Cow {
+
+    public static final int MIN_WALKSPEED = 10;
+    public static final int MAX_WALKSPEED = 100;
+
+    public Cow(){
+
+    }
+
 }

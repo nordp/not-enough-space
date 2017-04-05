e822ea5e84131f24804f514263db39e3bef31b11
Philip <nordp@student.chalmers.se>
2017-04-05 13:47:23 +0200

    Prelimenary work on CowControl

--- src/main/java/edu/chalmers/notenoughspace/CowControl.java
+++ src/main/java/edu/chalmers/notenoughspace/CowControl.java
@@ -1,56 +1,49 @@
 package edu.chalmers.notenoughspace;
 
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
         import com.jme3.math.Vector3f;
         import com.jme3.renderer.RenderManager;
         import com.jme3.renderer.ViewPort;
-        import com.jme3.scene.Node;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
         import com.jme3.scene.Spatial;
         import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.shape.Line;
 
 public class CowControl extends AbstractControl {
 
     private final static float REACTION_DISTANCE = 2f;
-    private final static float SPEED = 0.01f;
+    private final static float SPEED = 0.00001f;
 
     private Spatial shipModel;
-    private Vector3f direction = new Vector3f(0, 0, 1);
-    private boolean scared = false;
+
     private float x = FastMath.rand.nextFloat() * 0.001f;
     private float y = FastMath.rand.nextFloat() * 0.001f;
     private float z = FastMath.rand.nextFloat() * 0.001f;
 
 
-
     public CowControl(Node player) {
         this.shipModel = player.getChild(0);
+
     }
 
     @Override
     protected void controlUpdate(float tpf) {
+        Spatial cowModel = ((Node) spatial).getChild(0);
         Vector3f shipPos = shipModel.getWorldTranslation();
-        Vector3f cowPos = spatial.getWorldTranslation();
+        Vector3f cowPos = cowModel.getWorldTranslation();
+
         if (shipPos.distance(cowPos) < REACTION_DISTANCE) {
-            //pivotNode.rotate(0, 0.01f, 0);
-            Vector3f ballToShipVector = shipPos.subtract(cowPos);
-            Vector3f projectionVector = ballToShipVector.project(cowPos);
-            Vector3f newZAxis = ballToShipVector.subtract(projectionVector).normalizeLocal();
+            Vector3f distanceVector = shipPos.subtract(cowPos);
+            Vector3f projectionVector = distanceVector.project(cowPos);
+            Vector3f newZAxis = distanceVector.subtract(projectionVector).normalizeLocal();
 
-            spatial.lookAt(newZAxis, cowPos);
-            direction = newZAxis;
-            spatial.rotate(-SPEED, 0, 0);
-            scared = false;
+            spatial.lookAt(newZAxis, new Vector3f(0,1,0));
+
         } else {
-            if (scared) {
-                spatial.getParent().rotate(x, y, z);
-            } else {
-                x = -x;
-                y = -y;
-                z = -z;
-                scared = true;
-            }
+            spatial.rotate(0,0,0.002f);
         }
-
     }
 
     @Override

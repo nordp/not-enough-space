7155a079336afadb5f0342428b7a614f51613668
Philip <nordp@student.chalmers.se>
2017-04-05 13:46:53 +0200

    Fix cow spawning and rotation

--- src/main/java/edu/chalmers/notenoughspace/CowFactory.java
+++ src/main/java/edu/chalmers/notenoughspace/CowFactory.java
@@ -2,6 +2,7 @@ package edu.chalmers.notenoughspace;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.light.DirectionalLight;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
@@ -17,21 +18,18 @@ public class CowFactory {
     public CowFactory(AssetManager assetManager, Node player, float height){
         this.assetManager = assetManager;
         this.player = player;
-
-        //Spatial for model. "this"-node located in center of planet still.
         this.height = height;
     }
 
     public Cow createCow(){
+        //Spatial for model. "this"-node located in center of planet still.
         Cow cow = new Cow(assetManager, height);
-        cow.setLocalTranslation(0, height, 0);
         Spatial cowModel = assetManager.loadModel("Models/cow.obj");
-        cow.setModel(cowModel);
+        cowModel.setLocalTranslation(0, height, 0);
+        cowModel.rotate(FastMath.PI + FastMath.HALF_PI, FastMath.PI, 0);
         cowModel.setMaterial(assetManager.loadMaterial("Materials/CowMaterial.j3m"));
-
+        cow.attachChild(cowModel);
         cow.addControl(new CowControl(player));
-        DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
         return cow;
     }
 }

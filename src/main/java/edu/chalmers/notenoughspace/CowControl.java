b124650ac5abe242e6fc159266d2b21bbbd4d661
Philip <nordp@student.chalmers.se>
2017-04-23 20:08:23 +0200

    implemented mood-based walking patterns

--- src/main/java/edu/chalmers/notenoughspace/CowControl.java
+++ src/main/java/edu/chalmers/notenoughspace/CowControl.java
@@ -10,18 +10,23 @@ import com.jme3.scene.control.AbstractControl;
 
 public class CowControl extends AbstractControl {
 
-    private final static float REACTION_DISTANCE = 2f;
-    private final static float SPEED = 0.001f;
+    private final static float REACTION_DISTANCE = 3f;
+    private final static float SPEED = 0.1f;
+    private static final float SPRINT_SPEED = 0.3f;
+    private final static float MAX_DIR = 3;
+    private final static int SPRINT_COOLDOWN = 200;
+    private final static int MAX_STAMINA = 200;
 
     private Spatial shipModel;
 
-    private float x = FastMath.rand.nextFloat() * 0.001f;
-    private float y = FastMath.rand.nextFloat() * 0.001f;
-    private float z = FastMath.rand.nextFloat() * 0.001f;
-
+    private float walkDir;
+    private int stamina;
+    private CowMood mood;
 
     public CowControl(Ship ship) {
         this.shipModel = ship.getChild(0);
+        mood = CowMood.CALM;
+        stamina = MAX_STAMINA;
     }
 
     @Override
@@ -31,23 +36,64 @@ public class CowControl extends AbstractControl {
         Vector3f cowPos = cowModel.getWorldTranslation();
 
         if (shipPos.distance(cowPos) < REACTION_DISTANCE) {
-            Vector3f distanceVector = shipPos.subtract(cowPos);
-            Vector3f projectionVector = distanceVector.project(cowPos);
-            Vector3f newZAxis = distanceVector.subtract(projectionVector).negate();
-            spatial.lookAt(newZAxis, cowPos);
+            mood = CowMood.SCARED;
+        } else {
+            mood = CowMood.CALM;
+        }
+        if (stamina < 0){
+            mood = CowMood.TIRED;
+            if (stamina < -SPRINT_COOLDOWN){
+                stamina = MAX_STAMINA;
+            }
         }
 
-        walk();
-    }
 
-    private void walk(){
-        int rand = (int)FastMath.rand.nextFloat()*100;
-        if (rand < 25) {
-            spatial.rotate(SPEED,0,0);
-        } else if (rand < 75) {
-            spatial.rotate(0,SPEED*200,0);
-        } else {
-            spatial.rotate(0,0,SPEED*200);
+        switch (mood){
+            case CALM:
+
+                //Change walkDir twice in 100.
+                float rand = FastMath.rand.nextFloat();
+                if (rand*100 < 2) {
+                    walkDir = FastMath.DEG_TO_RAD*(FastMath.rand.nextFloat()*MAX_DIR-MAX_DIR/2);
+                }
+                //Walk
+                spatial.rotate(SPEED*tpf,walkDir,0);
+
+                break;
+
+            case SCARED:
+                /*Vector3f distanceVector = shipPos.subtract(cowPos);
+                Vector3f projectionVector = distanceVector.project(cowPos);
+                Vector3f newZAxis = distanceVector.subtract(projectionVector).negate();
+                spatial.lookAt(newZAxis, cowPos);
+                spatial.rotate((FastMath.DEG_TO_RAD*-60),0,0);
+                */
+
+
+                //Turn left or right?
+                Spatial left = ((Node)spatial.clone()).getChild(0);
+                Spatial right = ((Node)spatial.clone()).getChild(0);
+
+                left.getParent().rotate(SPEED*tpf,FastMath.DEG_TO_RAD*MAX_DIR,0);
+                right.getParent().rotate(SPEED*tpf,FastMath.DEG_TO_RAD*-MAX_DIR,0);
+
+                float sprintDir;
+                if (left.getWorldTranslation().distance(shipModel.getWorldTranslation()) <
+                        right.getWorldTranslation().distance(shipModel.getWorldTranslation())){
+                    sprintDir = -MAX_DIR;
+                } else {
+                    sprintDir = MAX_DIR;
+                }
+
+                spatial.rotate(SPRINT_SPEED*tpf, FastMath.DEG_TO_RAD*sprintDir, 0);
+                stamina--;
+                break;
+
+            case TIRED:
+                //Walk
+                spatial.rotate(SPEED/2*tpf,walkDir/2*tpf,0);
+                stamina--;
+                break;
         }
     }
 

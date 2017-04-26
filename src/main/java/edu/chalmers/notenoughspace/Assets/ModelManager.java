fb5415df1d619660eb1dd189c7bd394f00d24c44
juliaortheden <orthedenjulia@gmail.com>
2017-04-26 09:51:17 +0200

    Created modelmanager

---
+++ src/main/java/edu/chalmers/notenoughspace/Assets/ModelManager.java
@@ -0,0 +1,20 @@
+package edu.chalmers.notenoughspace.Assets;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.math.FastMath;
+import com.jme3.scene.Spatial;
+
+
+class ModelManager {
+
+    private AssetManager assetManager;
+
+    public Spatial LoadModel(String modelId) {
+        Spatial model= null;
+        if (modelId.equals("cow")) {
+            Spatial cowModel = assetManager.loadModel("Models/cow.obj");
+            cowModel.rotate(FastMath.HALF_PI, FastMath.HALF_PI, FastMath.PI);
+        }
+        return model;
+    }
+}

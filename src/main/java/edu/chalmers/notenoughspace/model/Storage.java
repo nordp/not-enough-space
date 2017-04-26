b2f81360492c177a8cad5d98a6b4f2ccf3ebc1a9
juliaortheden <orthedenjulia@gmail.com>
2017-04-26 19:18:27 +0200

    Added list of beamable and getWeight

--- src/main/java/edu/chalmers/notenoughspace/model/Storage.java
+++ src/main/java/edu/chalmers/notenoughspace/model/Storage.java
@@ -1,7 +1,25 @@
 package edu.chalmers.notenoughspace.model;
 
+import edu.chalmers.notenoughspace.nodes.Beamable;
+
+import java.util.LinkedList;
+import java.util.*;
+
 /**
  * Created by juliaortheden on 2017-04-06.
  */
 public class Storage {
+
+    private List<Beamable> beamableList = new LinkedList();
+
+    public void calculateScore(){
+    }
+
+    public int calculateWeight() {
+        int weight = 0;
+        for (int i = 0; i < beamableList.size(); i++) {
+            weight += beamableList.get(i).getWeight();
+        }
+     return weight;
+    }
 }

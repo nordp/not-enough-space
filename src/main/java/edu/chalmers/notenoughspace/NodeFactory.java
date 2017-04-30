393f1788ca7b6e9fd2352e7f737f9d004617f492
Philip <nordp@student.chalmers.se>
2017-04-30 14:51:18 +0200

    Fix Satellite bug

--- src/main/java/edu/chalmers/notenoughspace/NodeFactory.java
+++ src/main/java/edu/chalmers/notenoughspace/NodeFactory.java
@@ -71,7 +71,7 @@ public class NodeFactory {
         satelliteNode.attachChild(satelliteModel);
         satelliteNode.addControl(new SatelliteControl());
 
-        return satelliteModel;
+        return satelliteNode;
     }
 
     //public Spatial createPlanet(){} TODO

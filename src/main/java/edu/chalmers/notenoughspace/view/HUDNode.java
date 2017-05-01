1f2078063f30128e7e368b0fca9567ec00b5c9cf
Philip <nordp@student.chalmers.se>
2017-05-01 22:03:32 +0200

    Initial HUD elements

--- src/main/java/edu/chalmers/notenoughspace/view/HUDNode.java
+++ src/main/java/edu/chalmers/notenoughspace/view/HUDNode.java
@@ -4,6 +4,7 @@ import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
 import com.jme3.scene.Node;
 import edu.chalmers.notenoughspace.assets.ModelLoaderFactory;
+import edu.chalmers.notenoughspace.model.Storage;
 import edu.chalmers.notenoughspace.util.StringFormatUtil;
 
 /**
@@ -12,6 +13,8 @@ import edu.chalmers.notenoughspace.util.StringFormatUtil;
 public class HUDNode extends Node {
 
     BitmapText timerText;
+    BitmapText scoreText;
+    BitmapText weightText;
 
     public HUDNode(int height, int width){
         //elapsedTime = 0;
@@ -23,17 +26,28 @@ public class HUDNode extends Node {
         attachChild(timerText);
         
         
-        BitmapText txt = new BitmapText(ModelLoaderFactory.getFontLoader().loadFont());
-        txt.setText("Hello HUD");
-        txt.setSize(50);
-        txt.move(0,height,0);
-        attachChild(txt);
+        scoreText = new BitmapText(font);
+        scoreText.setText("Poäng: NULL");
+        scoreText.setSize(25);
+        scoreText.move(0,height,0);
+        attachChild(scoreText);
+
+        weightText = new BitmapText(font);
+        weightText.setText("NULL kg");
+        weightText.setSize(25);
+        weightText.move(0,height-scoreText.getLineHeight(),0);
+        attachChild(weightText);
     }
 
 
     public void updateTimer(float seconds){             //TODO: Make HUD listen to changes in model by observer pattern
         timerText.setText("Time left: " +
                 StringFormatUtil.toTimeFormat(seconds));
+    }
+
+    public void updateStorage(Storage storage){
+        scoreText.setText("Poäng: " + storage.calculateScore());
+        weightText.setText(storage.calculateWeight() + "kg");
     }
 
 }

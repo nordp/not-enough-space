d331df3fc39028ebb346ae65d0abb30b37e79cbb
Philip <nordp@student.chalmers.se>
2017-05-10 12:28:44 +0200

    Created separate level class and GameOverEvent

---
+++ src/main/java/edu/chalmers/notenoughspace/core/Level.java
@@ -0,0 +1,50 @@
+package edu.chalmers.notenoughspace.core;
+
+import edu.chalmers.notenoughspace.event.Bus;
+import edu.chalmers.notenoughspace.event.GameOverEvent;
+import edu.chalmers.notenoughspace.util.CountDownTimer;
+
+public class Level {
+
+
+    private final int LEVEL_TIME = 120; //seconds
+
+    private CountDownTimer timer; //The total time the round has been active, in seconds.
+
+    private Ship ship;
+
+    private Planet planet;
+
+
+    public Level() {
+        ship = new Ship();
+        planet = new Planet();
+        //Test population
+        planet.populate(10, 10, 1);
+        //Init timer.
+        timer = new CountDownTimer(LEVEL_TIME) {
+            @Override
+            public void onTimeOut() {
+                levelOver();
+            }
+        };
+    }
+
+    public void update(float tpf) {
+        timer.tick(tpf/1000);
+    }
+
+    private void levelOver() {
+        Bus.getInstance().post(new GameOverEvent(this));
+    }
+
+    /**
+     * @return time left on level in seconds
+     */
+    public float getTimeLeft() {
+        return timer.getTimeLeft();
+    }
+
+    public void start() {
+    }
+}
---
+++ src/main/java/edu/chalmers/notenoughspace/event/GameOverEvent.java
@@ -0,0 +1,12 @@
+package edu.chalmers.notenoughspace.event;
+
+import edu.chalmers.notenoughspace.core.Level;
+import edu.chalmers.notenoughspace.util.CountDownTimer;
+
+/**
+ * Created by Phnor on 2017-05-10.
+ */
+public class GameOverEvent {
+    public GameOverEvent(Level level) {
+    }
+}
